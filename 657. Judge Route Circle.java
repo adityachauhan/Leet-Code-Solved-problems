e79a2622934c1079468e95a717de7daec3c378e8
Aditya Chauhan <chauhanaditya3628@gmail.com>
2018-06-26 03:22:40 +0100

    Create 657. Judge Route Circle.java

---
+++ 657. Judge Route Circle.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean judgeCircle(String moves) {
+        int x=0, y=0;
+        //boolean res = true;
+        for(int i=0;i<moves.length();i++){
+            if(moves.charAt(i)=='U') y++;
+            else if(moves.charAt(i)=='D') y--;
+            else if(moves.charAt(i)=='L') x--;
+            else x++;
+        }
+        return x==0 && y==0;
+    }
+}
